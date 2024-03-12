717736a97263f9c82e2050c3ab13f40b19abe5aa
Koyuncu <130415130+koyuncu5362@users.noreply.github.com>
2024-03-12 21:36:04 +0300

    Customer Register Operation Created

---
+++ src/demoProject/Application/Features/Auth/Commands/CustomerRegister/CustomerRegisteredResponse.cs
@@ -0,0 +1,13 @@
+﻿using Core.Application.Responses;
+using Core.Security.JWT;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Auth.Commands.CustomerRegister;
+public class CustomerRegisteredResponse : IResponse
+{
+    
+}
